c26ef2a3c8a84c96b53d544fd9c57cdafe4d4acd
kawser <kawserarasel24@gmail.com>
2022-09-18 12:26:22 +0600

    Setting fragment added Offer Fragment added plus 4 fragment added

--- app/src/main/java/com/ahamed/digitalkot/HomeFragment.java
+++ app/src/main/java/com/ahamed/digitalkot/HomeFragment.java
@@ -1,10 +1,11 @@
 package com.ahamed.digitalkot;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.view.ContextMenu;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -12,17 +13,39 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import com.ahamed.digitalkot.databinding.FragmentHomeBinding;
 
 
 public class HomeFragment extends Fragment {
 
-
     public HomeFragment() {
         // Required empty public constructor
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        setHasOptionsMenu(true);
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        inflater.inflate(R.menu.my_menu, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.id_settings) {
+            if (getView() != null) {
+                Navigation.findNavController(getView()).navigate(R.id.action_homeToSetting);
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
