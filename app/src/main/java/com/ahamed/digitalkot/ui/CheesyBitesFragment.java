6d9a3704396efd879404c13594bdb75c4b5bfafe
kawser <kawserarasel24@gmail.com>
2022-09-21 14:25:11 +0600

    Cheesy Bite dialog added with java code and XML

--- app/src/main/java/com/ahamed/digitalkot/ui/CheesyBitesFragment.java
+++ app/src/main/java/com/ahamed/digitalkot/ui/CheesyBitesFragment.java
@@ -1,74 +1,45 @@
 package com.ahamed.digitalkot.ui;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
-import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.ahamed.digitalkot.R;
-import com.ahamed.digitalkot.adapter.PanPizzaAdapter;
-import com.ahamed.digitalkot.databinding.FragmentPanBinding;
-import com.ahamed.digitalkot.entites.Pan;
-import com.ahamed.digitalkot.listener.PizzaListener;
-import com.ahamed.digitalkot.viewmodel.PanViewModel;
+import com.ahamed.digitalkot.databinding.FragmentCheesyBitesBinding;
 
-import java.util.ArrayList;
-import java.util.List;
+public class CheesyBitesFragment extends Fragment {
+    private AlertDialog alertDialog;
 
-
-public class PanFragment extends Fragment implements PizzaListener {
-    AlertDialog alertDialog;
-    PanViewModel viewModel;
-    PanPizzaAdapter adapter;
-    List<Pan> pizzaList;
-
-    public PanFragment() {
+    public CheesyBitesFragment() {
         // Required empty public constructor
     }
 
-    @SuppressLint("NotifyDataSetChanged")
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        FragmentPanBinding binding = FragmentPanBinding.inflate(inflater);
-        pizzaList = new ArrayList<>();
+        FragmentCheesyBitesBinding binding = FragmentCheesyBitesBinding.inflate(inflater, container, false);
 
         AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
-        View dialogView = getLayoutInflater().inflate(R.layout.dailog_add_pan_pizza, null);
+        View dialogView = getLayoutInflater().inflate(R.layout.dailog_add_cb, null);
         builder.setView(dialogView);
         alertDialog = builder.create();
-
-        binding.rvAllPan.setLayoutManager(new LinearLayoutManager(getActivity()));
-        adapter = new PanPizzaAdapter(pizzaList, this);
-        binding.rvAllPan.setAdapter(adapter);
-
-        viewModel = new ViewModelProvider(requireActivity()).get(PanViewModel.class);
-        viewModel.getAllPanPizza().observe(getViewLifecycleOwner(), pans -> {
-            pizzaList.clear();
-            pizzaList.addAll(pans);
-            adapter.notifyDataSetChanged();
-        });
-
         binding.btnAdd.setOnClickListener(view -> alertDialog.show());
 
-        Button submit = dialogView.findViewById(R.id.btn_ok);
-        Button cancel = dialogView.findViewById(R.id.btn_cancel);
-        EditText name = dialogView.findViewById(R.id.tv_item_name);
-        EditText ed_personal = dialogView.findViewById(R.id.tv_personal_price);
-        EditText ed_medium = dialogView.findViewById(R.id.tv_medium_price);
-        EditText ed_family = dialogView.findViewById(R.id.tv_family_price);
+        Button submit = dialogView.findViewById(R.id.cv_btn_ok);
+        Button cancel = dialogView.findViewById(R.id.cv_btn_cancel);
+        EditText name = dialogView.findViewById(R.id.tv_cv_name);
+        EditText ed_personal = dialogView.findViewById(R.id.cv_personal_price);
+        EditText ed_medium = dialogView.findViewById(R.id.cv_medium_price);
+        EditText ed_family = dialogView.findViewById(R.id.cv_family_price);
 
         submit.setOnClickListener(view -> {
             int personal;
@@ -115,18 +86,12 @@ public class PanFragment extends Fragment implements PizzaListener {
             ed_medium.setText(null);
             ed_family.setText(null);
         });
+
+
         return binding.getRoot();
     }
 
     private void dataSend(String itemName, int personal, int medium, int family) {
-        Pan panPizza = new Pan(itemName, personal, medium, family);
-        Log.d("TAG", "dataSend: " + itemName + personal + medium + family);
-        viewModel.addPan(panPizza);
-        Toast.makeText(getActivity(), "Item Added", Toast.LENGTH_SHORT).show();
-    }
 
-    @Override
-    public void actionListener(Pan pan) {
-        viewModel.deletePan(pan);
     }
 }
