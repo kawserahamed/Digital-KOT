ecd3caa5e2bec0998ca444d9dd8209ec7fc965cc
HP <kawserarasel24@gmail.com>
2022-10-09 18:21:24 +0600

    Some updated

--- app/src/main/java/com/ahamed/digitalkot/AddCustomerFragment.java
+++ app/src/main/java/com/ahamed/digitalkot/AddCustomerFragment.java
@@ -1,6 +1,5 @@
 package com.ahamed.digitalkot;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -14,7 +13,6 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.ahamed.digitalkot.adapter.RvPanAdapter;
-import com.ahamed.digitalkot.databinding.DialogPanBinding;
 import com.ahamed.digitalkot.databinding.FragmentAddCustomerBinding;
 import com.ahamed.digitalkot.entites.Pan;
 import com.ahamed.digitalkot.viewmodel.PanViewModel;
@@ -45,7 +43,8 @@ public class AddCustomerFragment extends Fragment {
         AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
         View dialogView = getLayoutInflater().inflate(R.layout.dialog_pan, null);
         recyclerView = dialogView.findViewById(R.id.rv_pan);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));;
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        ;
         adapter = new RvPanAdapter(pizzaList);
         recyclerView.setAdapter(adapter);
         builder.setView(dialogView);
@@ -53,7 +52,7 @@ public class AddCustomerFragment extends Fragment {
 
         viewModel = new ViewModelProvider(requireActivity()).get(PanViewModel.class);
         viewModel.getAllPanPizza().observe(getViewLifecycleOwner(), pans -> {
-            Log.d("TAG", "onClick: "+pans.size());
+            Log.d("TAG", "onClick: " + pans.size());
             pizzaList.clear();
             pizzaList.addAll(pans);
             adapter.notifyDataSetChanged();
@@ -65,7 +64,6 @@ public class AddCustomerFragment extends Fragment {
             public void onClick(View view) {
 
                 alertDialog.show();
-
 
 
             }
